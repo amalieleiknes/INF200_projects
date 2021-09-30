b71092ca1f1f56ae1c2872df3309bbf0b368c2ed
Amalie Christine Leiknes <amalie.leiknes@hotmail.com>
2021-09-30 12:58:07 +0200

    Oppgv 1 ok

---
+++ Project_3/marking_errors.py
@@ -0,0 +1,39 @@
+import re
+
+'''
+def markingErrors(inputSentence):
+    print(inputSentence)
+    matches = []
+    for match in re.finditer(r"\b(\w+)\s+\1\b", inputSentence):
+        matches += [match.start(), match.end()]
+
+    for m in matches:
+        print(m)
+
+
+markingErrors("I can see the the red rose rose and the purple lilac.")
+
+
+
+'''
+import re
+
+
+def markingErrors(inputSentence):
+    print(inputSentence)
+    count = 0
+    for match in re.finditer(r"\b(\w+)\s+\1\b", inputSentence):
+        startNext = match.start()
+        if count == 0:
+            print(' ' * match.start() + '*' * (match.end() - match.start()), end='')
+            count += 1
+        else:
+            print((startNext-endPrev)*" ", end='')
+            print('' * match.start() + '*' * (match.end() - match.start()), end='')
+        endPrev = match.end()
+
+markingErrors("I can see the the red rose rose and the purple lilac.")
+
+print()
+
+markingErrors("I I I I")
